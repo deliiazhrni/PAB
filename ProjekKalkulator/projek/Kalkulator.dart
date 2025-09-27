988f39c6f281672f49f5e3d0fddce671d8031d9d
deliiazhrni <deliazahrani_2428240111@mhs.mdp.ac.id>
2025-09-27 07:19:50 +0700

    Add files via upload

---
+++ ProjekKalkulator/projek/Kalkulator.dart
@@ -0,0 +1,17 @@
+class Kalkulator{
+  double penambahan(double a, double b){
+    return a + b;
+  }
+
+  double pengurangan (double a, double b){
+    return a - b;
+  }
+
+  double pembagian (double a, double b){
+    return a / b;
+  }
+
+  double perkalian(double a, double b){
+    return a * b;
+  }
+}
